337f99c770438a6f8d34a3e0c40567ee7ac673f2
Maxwell Chow <maxchowla@gmail.com>
2016-07-15 15:18:32 +0800

    calling login

--- bot.py
+++ bot.py
@@ -1,11 +1,3 @@
-import praw, re
-r = praw.Reddit()
+import praw, re, obot
 
-f = open('secrets.txt','r')
-pword = re.sub('\n', '', f.readline())
-secret = re.sub('\n', '', f.readline())
-r.set_oauth_app_info(client_id='-CW-pzNvWiIbXw',
-    client_secret=secret,
-    redirect_uri='http://127.0.0.1:65010/authorize_callback')
-url = r.get_authorize_url('uniqueKey','identityKey', True)
-import url
+r = obot.login()
