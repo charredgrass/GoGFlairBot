74ea9b7a86f0e3406bface6987cb678ad9cb0974
Maxwell Chow <maxchowla@gmail.com>
2016-07-15 16:27:59 +0800

    added some re stuff

--- bot.py
+++ bot.py
@@ -13,12 +13,6 @@ def parseComment(c):
     currflair_class = c.author_flair_text
     text = c.body
     #check body against regex. if fails, return.
-    re_correct_post = re.compile(r"^Gifted or Grabbed: (?:Gifted|Grabbed)\n\nGame received\/gifted: [a-zA-Z ]+\n\nLink to your steam profile: http(?:s)?:\/\/steamcommunity\.com(?:\/id\/[a-zA-Z]+)\n\nLinks to \[GoG\] \+ giveaway: .*\n\nTotal Grabbed now: \d+")
-    print(c.body)
-    if re_correct_post.match(text):
-        print("REEEE")
-    else:
-        print("ROOOO")
 
 for x in submission.comments:
     parseComment(x)
